0071e0d7c2fbd85eccb9387324bd664f08e67777
Abdulla Al Rakib <36118180+abi30@users.noreply.github.com>
2021-03-10 13:49:47 +0100

    basic_class_done

--- Basic_2/basic_2.js
+++ Basic_2/basic_2.js
@@ -1,10 +1,4 @@
 
-
-
-
-
-
-
 class car{
 
     constructor(name, mdl, dr, clr){
@@ -12,41 +6,59 @@ class car{
         this. model = mdl;
         this. doors = dr;
         this. color = clr;
+    }
 
-
-    about: function() {
-    return this.BrandName +' is one of the most mordern car and it has ' + this.doors +'doors';
-
-
+    about() {
+    return this.BrandName +' is one of the most mordern car and it has ' + this.doors +' doors';
 
 
 }
-about(){
+
 
 }
 
-}
+
+var car1 =new car("BMW","570i",2,"black");
+
+
+console.log(car1.about());
 
 
 
 class animal{
 
-    constructor(){
+    constructor(name,orgin_country,weight,clr){
+
+        this.Name=name;
+        this.from=orgin_country;
+        this.Weight=weight;
+        this.color=clr;
 
     }
 
-    about(){
-
+    living(){
+            return `${this.Name} is living in ${this.from}`;
     }
 
 }
+
+
+var anml1= new animal("tiger","bangladesh",70,"white");
+
+
+console.log(anml1.living());
+
+
+
+
 class Person {
     
-    constructor(name, age, jobTitle, gym){
+     constructor(name, age, jobTitle, gym){
         this.fName = name;
         this.age= age;
         this.jobTitle = jobTitle;
         this.gym = gym;
+      
     }
     showMessage(){
         return `Hey, My name is ${this.fName}`;
@@ -55,8 +67,9 @@ class Person {
 
 
 
+var per1 =new Person("gregor",30,"developer",false);
 
-
+console.log(per1.showMessage());
 
 
 
